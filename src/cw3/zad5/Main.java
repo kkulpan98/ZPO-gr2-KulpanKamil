de9ccfb266cff4b7ff889f469e9e64f11a1a2b5f
kamkul <kkulpan98@gmail.com>
2021-06-07 22:20:00 +0200

    kolejne zadanka

---
+++ src/cw3/zad5/Main.java
@@ -0,0 +1,9 @@
+package cw3.zad5;
+
+public class Main {
+    public static void main(String[] args) {
+        Spolka spolka1 = new Spolka();
+        Spolka spolka2 = new Spolka();
+        Spolka spolka3 = new Spolka();
+    }
+}
